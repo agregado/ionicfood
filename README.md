cbdaa0fdecca27306f05075509b8af1b2fd13416
agregado <cristovammartins@outlook.com>
2018-11-01 00:17:01 -0300

    projeto completo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ionicfood
+# Pizza App
+
+<img src="screenshots/splash.png"  width="300"/>
+<img src="screenshots/home.png"  width="300"/>
+<img src="screenshots/pizza.png"  width="300"/>
+<img src="screenshots/profile.png"  width="300"/>
---
+++ src/configs/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBcr4scSGCOgCdkClLbtolBYtNLqkPjfi8",
+    authDomain: "ionic270118.firebaseapp.com",
+    databaseURL: "https://ionic270118.firebaseio.com",
+    projectId: "ionic270118",
+    storageBucket: "ionic270118.appspot.com",
+    messagingSenderId: "184180953265"
+}
